412c0a14c0e2a5da07415bc7000491d788bde957
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-03 14:05:30 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ant, prox;
+        int posicao;
+
+        Console.WriteLine("Informe até qual posição da sequencia deseja ver: ");
+        posicao = int.Parse(Console.ReadLine());
+
+        ant = 1;
+        prox = 0;
+        if (posicao == 1)
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            for (; posicao > 0; posicao --)
+            {
+                Console.Write((ant + prox) + " ");
+                prox = ant + prox;
+                ant = prox - ant;
+            }
+        }
+    }
+}
